c11da6c5218c7ee430580877d8a9f8feed56541c
Dim <dimitri.kiavue@gmail.com>
2020-06-02 10:42:41 +0200

    [ADD] Devise Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_devise_parameters, if: :devise_controller?
+  before_action :authenticate_user!, notice: 'Vous devez vous connecter pour accéder à ce contenu'
+
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u|
+    u.permit(
+             :first_name,
+             :last_name,
+             :username,
+             :email,
+             :password,
+             :password_confirmation
+             )
+           }
+  end
 end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
+
+  def home
+
+  end
+end
